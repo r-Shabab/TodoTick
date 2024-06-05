33ff99f4c7dca45e8a75722a2152b83ee6730736
Md Rashid Shabab <raashidshabab@gmail.com>
2024-06-05 12:21:19 +0600

    checkbox component added

--- src/components/Tasklist.jsx
+++ src/components/Tasklist.jsx
@@ -1,27 +1,32 @@
+import FramerCheckbox from "./FramerCheckbox";
+
 export default function Tasklist() {
+  const tasks = [
+    { id: "c-1", label: "Walk the dog" },
+    { id: "c-2", label: "Go outside" },
+    { id: "c-3", label: "Learn JS" },
+    { id: "c-4", label: "Dinner with family" },
+    { id: "c-5", label: "Call a friend" },
+  ];
+
   return (
     <section>
       <div className="w-full space-y-5 p-4">
         <h2 className="text-center text-xl font-bold underline decoration-emerald-400 underline-offset-4">
           Inbox
         </h2>
-        <div className="rounded-md bg-emerald-100 p-3 shadow-lg">
-          <ul className="space-y-5 rounded-lg bg-blue-200">
-            <li className="rounded-lg bg-red-300">
-              <label htmlFor="list1" className="h-2/12 w-2/12 bg-slate-100">
-                <input
-                  type="checkbox"
-                  name="list1"
-                  id="list1"
-                  className="appearance-none"
-                />
-              </label>
-              Walk the dog
-            </li>
-            <li>Go outside</li>
-            <li>Learn JS</li>
-            <li>Dinner with family</li>
-            <li>Call a friend</li>
+        <div className="rounded-md bg-white p-3 shadow-lg">
+          <ul className="space-y-5 rounded-lg bg-white">
+            {tasks.map((task) => (
+              <li key={task.id} className="rounded-lg">
+                <div className="flex items-center p-4">
+                  <FramerCheckbox id={task.id}>
+                    <FramerCheckbox.Indicator />
+                    <FramerCheckbox.Label>{task.label}</FramerCheckbox.Label>
+                  </FramerCheckbox>
+                </div>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
