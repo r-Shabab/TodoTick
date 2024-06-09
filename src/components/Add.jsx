61dd03e855b3f05f9553e76f4d2a8d71bcc41240
Md Rashid Shabab <raashidshabab@gmail.com>
2024-06-09 10:25:08 +0600

    style: add task button

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -4,7 +4,7 @@ export default function Add() {
   return (
     <button
       className={
-        "fixed left-[78%] top-[85%] rounded-full bg-emerald-500 p-3.5 text-white shadow-xl hover:bg-emerald-800 hover:text-gray-200 hover:shadow-lg"
+        "fixed left-[78%] top-[85%] rounded-full bg-emerald-500 p-3.5 text-white shadow-xl hover:bg-emerald-800 hover:text-gray-200 hover:shadow-lg focus:ring focus:ring-emerald-500"
       }
     >
       <IoIosAdd className="h-10 w-10" />
