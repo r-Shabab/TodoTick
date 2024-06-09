182fedc5cd09ad0b7e68f1d3b7e436e8bb69f17a
Md Rashid Shabab <raashidshabab@gmail.com>
2024-06-09 10:12:14 +0600

    style: add focus status of edit & delete button

--- src/components/Delete.jsx
+++ src/components/Delete.jsx
@@ -2,8 +2,8 @@ import { AiFillDelete } from "react-icons/ai";
 export default function Delete() {
   return (
     <>
-      <button className="rounded-md bg-red-500 p-3 text-white hover:bg-red-800 hover:text-gray-200 hover:shadow-lg">
-        <AiFillDelete className="h-7 w-7" />
+      <button className="rounded-md bg-red-500 p-3 text-white hover:bg-red-800 hover:text-gray-200 hover:shadow-lg focus:outline-none focus:ring focus:ring-red-500">
+        <AiFillDelete className="h-5 w-5" />
       </button>
     </>
   );
--- src/components/Edit.jsx
+++ src/components/Edit.jsx
@@ -2,8 +2,8 @@ import { FaRegEdit } from "react-icons/fa";
 export default function Edit() {
   return (
     <>
-      <button className="rounded-md bg-blue-600 p-3 text-white shadow-xl hover:bg-blue-800 hover:text-gray-200 hover:shadow-lg">
-        <FaRegEdit className="h-7 w-7" />
+      <button className="rounded-md bg-blue-600 p-3 text-white shadow-xl hover:bg-blue-800 hover:text-gray-200 hover:shadow-lg focus:outline-none focus:ring focus:ring-blue-600">
+        <FaRegEdit className="h-5 w-5" />
       </button>
     </>
   );
