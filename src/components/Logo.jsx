5c2746dfde40d166eb280623a886bd213127f0ab
Md Rashid Shabab <raashidshabab@gmail.com>
2024-06-10 12:14:26 +0600

    update tailwind config

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,10 +2,10 @@ import { LuListTodo } from "react-icons/lu";
 export const Logo = () => {
   return (
     <div id="logo" className="flex items-center gap-2 hover:cursor-pointer">
-      <span className="text-primary1 font-dosis text-xl font-bold lg:text-4xl">
+      <span className="font-title text-xl font-bold text-primary1 lg:text-4xl">
         TodoTick
       </span>
-      <LuListTodo className="bg-primary1 h-7 w-7 rounded-full px-1 text-white lg:h-12 lg:w-12" />
+      <LuListTodo className="h-7 w-7 rounded-full bg-primary1 px-1 text-white lg:h-12 lg:w-12" />
     </div>
   );
 };
