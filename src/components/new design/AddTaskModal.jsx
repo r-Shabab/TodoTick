e68f12dce1fa64202a3ff06e7e82877004bb6e07
Md Rashid Shabab <raashidshabab@gmail.com>
2024-06-19 22:10:05 +0600

    feat: priority

--- src/components/new design/AddTaskModal.jsx
+++ src/components/new design/AddTaskModal.jsx
@@ -4,23 +4,34 @@ export const AddTaskModal = ({ show, onClose, onSave }) => {
   if (!show) return null;
 
   const [task, setTask] = React.useState('');
+  const [priority, setPriority] = React.useState('None');
 
   const handleSave = () => {
     onSave(task);
     setTask('');
+    setPriority('None');
   };
 
-  return (
-    <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center z-50">
+  return (<div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center z-50">
       <div className="bg-white p-5 rounded-lg shadow-lg w-1/3">
         <h2 className="text-lg font-semibold mb-4">Add Task</h2>
-        <input
-          type="text"
+        <textarea
           className="w-full p-2 border border-gray-300 rounded mb-4"
-          placeholder="Add a new task"
+          placeholder="Enter your task"
           value={task}
           onChange={(e) => setTask(e.target.value)}
-        />
+        ></textarea>
+        <select
+          className="w-1/5 p-2 border bg-priority-btn text-white border-gray-300 rounded mb-4"
+          value={priority}
+          onChange={(e) => setPriority(e.target.value)}
+          
+        >
+          <option value="None">None</option>
+          <option value="Low">Low</option>
+          <option value="Medium">Medium</option>
+          <option value="High">High</option>
+        </select>
         <div className="flex justify-end">
           <button
             className="bg-red-500 text-white px-4 py-2 rounded mr-2"
