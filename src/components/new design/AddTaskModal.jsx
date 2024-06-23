5dd6bc831c0be7c134764cb59bcdc1d6420f4af5
Md Rashid Shabab <raashidshabab@gmail.com>
2024-06-23 10:50:48 +0600

    fix error

--- src/components/new design/AddTaskModal.jsx
+++ src/components/new design/AddTaskModal.jsx
@@ -1,46 +1,51 @@
-import React from 'react'
+import { useState } from "react";
 
+// eslint-disable-next-line react/prop-types
 export const AddTaskModal = ({ show, onClose, onSave }) => {
+  const [task, setTask] = useState("");
+  const [priority, setPriority] = useState("Low");
   if (!show) return null;
-
-  const [task, setTask] = React.useState('');
-  const [priority, setPriority] = React.useState('Low');
 
   const handleSave = () => {
     onSave(task);
-    setTask('');
-    setPriority('Low');
+    setTask("");
+    setPriority("Low");
   };
 
-  return (<div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center z-50">
-      <div className="bg-white p-5 rounded-lg shadow-lg w-1/3">
-        <h2 className="text-lg font-semibold mb-4">Add Task</h2>
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-600 bg-opacity-50">
+      <div className="w-1/3 rounded-lg bg-white p-5 shadow-lg">
+        <h2 className="mb-4 text-lg font-semibold">Add Task</h2>
         <textarea
-          className="w-full p-2 border border-gray-300 rounded mb-4"
+          className="mb-4 w-full rounded border border-gray-300 p-2"
           placeholder="Enter your task"
           value={task}
           onChange={(e) => setTask(e.target.value)}
         ></textarea>
         <select
-          className="w-1/5 p-2 border bg-priority-btn text-white border-gray-300 rounded mb-4"
+          className="mb-4 w-1/5 rounded border border-gray-300 bg-priority-btn p-2 text-white"
           value={priority}
           onChange={(e) => setPriority(e.target.value)}
-          
         >
-          
-          <option value="Low" className='text-gray-400'>Low</option>
-          <option value="Medium" className='text-yellow-400'>Medium</option>
-          <option value="High" className='text-red-600'>High</option>
+          <option value="Low" className="text-gray-400">
+            Low
+          </option>
+          <option value="Medium" className="text-yellow-400">
+            Medium
+          </option>
+          <option value="High" className="text-red-600">
+            High
+          </option>
         </select>
         <div className="flex justify-end">
           <button
-            className="bg-red-500 text-white px-4 py-2 rounded mr-2"
+            className="mr-2 rounded bg-red-500 px-4 py-2 text-white"
             onClick={onClose}
           >
             Cancel
           </button>
           <button
-            className="bg-blue-500 text-white px-4 py-2 rounded"
+            className="rounded bg-blue-500 px-4 py-2 text-white"
             onClick={handleSave}
           >
             Save
@@ -49,4 +54,4 @@ export const AddTaskModal = ({ show, onClose, onSave }) => {
       </div>
     </div>
   );
-}
+};
