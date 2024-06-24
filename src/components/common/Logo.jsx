a6c8924c8204387e137a403be7eaec931524ceb3
Md Rashid Shabab <raashidshabab@gmail.com>
2024-06-24 17:59:00 +0600

    delete comments

--- src/components/common/Logo.jsx
+++ src/components/common/Logo.jsx
@@ -1,11 +1,7 @@
-// import LogoLight from "../../assets/LogoLight.png";
-// import LogoDark from "../../assets/LogoDark.png";
 import { AiOutlineCheckCircle } from "react-icons/ai";
 export const Logo = () => {
   return (
     <div className="flex items-center space-x-1">
-      {/* <img src={LogoLight} alt="" className="w-[60%]" />
-      <img src={LogoDark} alt="" className="hidden" /> */}
       <AiOutlineCheckCircle className="fill-color-primary-btn text-3xl transition-all duration-100 hover:fill-cyan-800" />
       <h1 className="font-logo text-4xl font-black tracking-tighter text-color-primary-btn transition-all duration-100 hover:text-cyan-800">
         TodoTick
