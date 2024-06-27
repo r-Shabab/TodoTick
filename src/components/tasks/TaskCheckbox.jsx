a4f86ff9074823477cfe30f322bdb8dbaaf9026a
Md Rashid Shabab <raashidshabab@gmail.com>
2024-06-27 17:58:41 +0600

    update checkbox layout

--- src/components/tasks/TaskCheckbox.jsx
+++ src/components/tasks/TaskCheckbox.jsx
@@ -30,7 +30,7 @@ const TaskCheckbox = () => {
     <div className="relative flex items-center">
       <input
         type="checkbox"
-        className="relative h-5 w-5 cursor-pointer appearance-none rounded-full border-2 border-gray-400 bg-gray-100 transition-all duration-500 checked:border-[#276792] checked:bg-color-primary-btn lg:h-6 lg:w-6"
+        className="relative h-5 w-5 cursor-pointer appearance-none rounded-full border-2 border-color-primary-btn bg-gray-100 transition-all duration-500 checked:border-[#276792] checked:bg-color-primary-btn lg:h-6 lg:w-6"
         onChange={handleCheckbox}
         id={taskId}
         checked={isChecked}
@@ -40,7 +40,7 @@ const TaskCheckbox = () => {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          strokeWidth="3.5"
+          strokeWidth="4"
           stroke="currentColor"
           className="h-3.5 w-3.5"
           initial={false}
